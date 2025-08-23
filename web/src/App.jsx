5cbf50f035660fd145272d61f72856c814db7b0d
Evan <evan.wang@tkspring.com>
2025-08-23 20:24:55 +0800

    web part

---
+++ web/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>EarnMax</h1>
+      <p>Frontend ready. Run `yarn dev` in the web/ folder to start the dev server.</p>
+    </div>
+  )
+}
